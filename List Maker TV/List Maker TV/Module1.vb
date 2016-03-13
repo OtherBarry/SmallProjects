badae46ff14404dd9e31902875636e7042cb7e08
OtherBarry <charlie.wilson4@gmail.com>
2016-03-13 17:01:44 +1100

    List Makers
    Makes a text document with all of the names of my movies and tv shows.
    Based on the folder names.

---
+++ List Maker TV/List Maker TV/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        My.Computer.FileSystem.DeleteFile("F:\TV\TV Shows.txt")
+        For Each foundShow As String In My.Computer.FileSystem.GetDirectories("F:\TV")
+            Dim showName As String = Replace(foundShow, "F:\TV\", "")
+            My.Computer.FileSystem.WriteAllText("F:\TV\TV Shows.txt", showName & vbCrLf, True)
+            For Each foundSeason As String In My.Computer.FileSystem.GetDirectories(foundShow)
+                foundSeason = Replace(foundSeason, foundShow + "\", "")
+                foundSeason = "   " + foundSeason & vbCrLf
+                My.Computer.FileSystem.WriteAllText("F:\TV\TV Shows.txt", foundSeason, True)
+            Next
+        Next
+    End Sub
+
+End Module
---
+++ List Maker/List Maker/Module1.vb
@@ -0,0 +1,58 @@
+﻿Module Module1
+
+    Sub Main()
+        My.Computer.FileSystem.DeleteFile("B:\Movies\Movies.txt")
+        My.Computer.FileSystem.WriteAllText("B:\Movies\Movies.txt", "Animated" & vbCrLf, True)
+        For Each foundFile As String In My.Computer.FileSystem.GetDirectories("B:\Movies\Animated")
+            If Char.IsPunctuation(foundFile.Chars(foundFile.Length - 1)) = False Then
+                Dim otherFile As String = Replace(foundFile, "B:\Movies\Animated\", "")
+                otherFile = "   " + otherFile & vbCrLf
+                My.Computer.FileSystem.WriteAllText("B:\Movies\Movies.txt", otherFile, True)
+                For Each moreFiles As String In My.Computer.FileSystem.GetDirectories(foundFile)
+                    moreFiles = Replace(moreFiles, foundFile + "\", "")
+                    moreFiles = "       " + moreFiles & vbCrLf
+                    My.Computer.FileSystem.WriteAllText("B:\Movies\Movies.txt", moreFiles, True)
+                Next
+            Else
+                foundFile = Replace(foundFile, "B:\Movies\Animated\", "")
+                foundFile = "   " + foundFile & vbCrLf
+                My.Computer.FileSystem.WriteAllText("B:\Movies\Movies.txt", foundFile, True)
+            End If
+        Next
+        My.Computer.FileSystem.WriteAllText("B:\Movies\Movies.txt", vbCrLf & "Documentaries" & vbCrLf, True)
+        For Each foundFile As String In My.Computer.FileSystem.GetDirectories("B:\Movies\Documentaries")
+            If Char.IsPunctuation(foundFile.Chars(foundFile.Length - 1)) = False Then
+                Dim otherFile As String = Replace(foundFile, "B:\Movies\Documentaries\", "")
+                otherFile = "   " + otherFile & vbCrLf
+                My.Computer.FileSystem.WriteAllText("B:\Movies\Movies.txt", otherFile, True)
+                For Each moreFiles As String In My.Computer.FileSystem.GetDirectories(foundFile)
+                    moreFiles = Replace(moreFiles, foundFile + "\", "")
+                    moreFiles = "       " + moreFiles & vbCrLf
+                    My.Computer.FileSystem.WriteAllText("B:\Movies\Movies.txt", moreFiles, True)
+                Next
+            Else
+                foundFile = Replace(foundFile, "B:\Movies\Documentaries\", "")
+                foundFile = "   " + foundFile & vbCrLf
+                My.Computer.FileSystem.WriteAllText("B:\Movies\Movies.txt", foundFile, True)
+            End If
+        Next
+        My.Computer.FileSystem.WriteAllText("B:\Movies\Movies.txt", vbCrLf & "Feature" & vbCrLf, True)
+        For Each foundFile As String In My.Computer.FileSystem.GetDirectories("B:\Movies\Feature")
+            If Char.IsPunctuation(foundFile.Chars(foundFile.Length - 1)) = False Then
+                Dim otherFile As String = Replace(foundFile, "B:\Movies\Feature\", "")
+                otherFile = "   " + otherFile & vbCrLf
+                My.Computer.FileSystem.WriteAllText("B:\Movies\Movies.txt", otherFile, True)
+                For Each moreFiles As String In My.Computer.FileSystem.GetDirectories(foundFile)
+                    moreFiles = Replace(moreFiles, foundFile + "\", "")
+                    moreFiles = "       " + moreFiles & vbCrLf
+                    My.Computer.FileSystem.WriteAllText("B:\Movies\Movies.txt", moreFiles, True)
+                Next
+            Else
+                foundFile = Replace(foundFile, "B:\Movies\Feature\", "")
+                foundFile = "   " + foundFile & vbCrLf
+                My.Computer.FileSystem.WriteAllText("B:\Movies\Movies.txt", foundFile, True)
+            End If
+        Next
+    End Sub
+
+End Module

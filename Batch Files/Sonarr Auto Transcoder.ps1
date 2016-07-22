1e122e39c76409f9eaff35be2048a439d30e91e7
OtherBarry <charlie.wilson4@gmail.com>
2016-07-22 14:06:25 +1000

    Replaced Retramo with a sonarr auto transcoder

--- Batch Files/Sonarr Auto Transcoder.ps1
+++
@@ -1,4 +0,0 @@
-$EpisodeFile_Path = $env:sonarr_episodefile_path
-$wshell = New-Object -ComObject Wscript.Shell
-$wshell.Popup("$EpisodeFile_Path",0,"Done",0x1)
-python "C:\Users\charl\Documents\MP4 Converter\manual.py" -i "$EpisodeFile_Path" -a
---
+++ SonarrAutoTranscoder/SonarrAutoTranscoder/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.IO
+Public Class Form1
+    Protected Overrides Sub SetVisibleCore(ByVal value As Boolean)
+        If Not Me.IsHandleCreated Then
+            Me.CreateHandle()
+            value = False
+        End If
+        Dim watchfolder As FileSystemWatcher
+        watchfolder = New FileSystemWatcher()
+        watchfolder.Path = "M:\TV Shows"
+        watchfolder.IncludeSubdirectories = True
+        watchfolder.NotifyFilter = (NotifyFilters.LastAccess Or NotifyFilters.LastWrite Or NotifyFilters.FileName Or NotifyFilters.DirectoryName)
+        AddHandler watchfolder.Created, AddressOf logchange
+        watchfolder.EnableRaisingEvents = True
+    End Sub
+    Private Sub logchange(ByVal source As Object, ByVal e As System.IO.FileSystemEventArgs)
+        Dim Transcode As New Process
+        Dim TranscodeInfo As New ProcessStartInfo("cmd.exe", "/C python ""C:\Users\charl\Documents\MP4 Converter\manual.py"" -a -i """ & e.FullPath & """")
+        TranscodeInfo.CreateNoWindow = True
+        TranscodeInfo.UseShellExecute = False
+        TranscodeInfo.RedirectStandardOutput = True
+        Transcode.StartInfo = TranscodeInfo
+        Transcode.Start()
+        Transcode.WaitForExit()
+    End Sub
+End Class
+
+
+
